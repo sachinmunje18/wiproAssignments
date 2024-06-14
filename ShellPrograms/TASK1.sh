eb4d69bb9bc2e544b43199eb3aef0ea561a726d7
Sachin Munje <117455620+sachinmunje18@users.noreply.github.com>
2024-06-14 09:08:39 +0530

    Update TASK1.sh

--- ShellPrograms/TASK1.sh
+++ ShellPrograms/TASK1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+filecheck()
+{
+file="$1"
+if [ -e $file ]
+then
+echo "File exists"
+else
+echo "File Not Found!"
+fi
+}
+
+read -p "Enter the File Name : " filename
+filecheck $filename
